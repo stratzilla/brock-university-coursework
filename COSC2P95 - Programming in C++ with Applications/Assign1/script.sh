7ba7e1d0e7cdb9bfe02494d0574c96a173702377
stratzilla <stratzillab@gmail.com>
2019-11-06 00:14:57 -0500

    Create script.sh

---
+++ COSC2P95 - Programming in C++ with Applications/Assign1/script.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "du --max-depth=1 >> somefile.txt"
+echo "touch -t 202401010000.00 something.txt"
+echo "\"this is text\" >> something.txt"
+echo "ln -s something.txt link"
+echo "ls P*"

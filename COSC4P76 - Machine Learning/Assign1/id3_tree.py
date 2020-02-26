e3c2f36eb56128eba5c3fea660bf6bfe16bbfcc6
stratzilla <stratzillab@gmail.com>
2020-02-26 18:08:32 -0500

    Update id3_tree.py

--- COSC4P76 - Machine Learning/Assign1/id3_tree.py
+++ COSC4P76 - Machine Learning/Assign1/id3_tree.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from sys import exit, argv
+import time
 
 try: # attempt to import dependencies
 	import pandas as pd
@@ -8,6 +9,16 @@ try: # attempt to import dependencies
 except ImportError: # if not found
 	print("\nModules could not be loaded.")
 	print("Ensure both `pandas` and `numpy` are installed before execution.\n")
+	exit(1)
+
+if len(argv) != 4: # if inappropriate arguments
+	print("\nExecute the script as below:\n")
+	print(" $ ./id3_tree_train.py <File> <Holdout> <Print>\n")
+	print("Where the arguments are as below:")
+	print(" <File> -- the .CSV file of examples")
+	print(" <Holdout> -- the proportion of training examples (0.00..1.00)")
+	print(" <Print> -- whether to print tree (1 = T, 0 = F)\n")
+	print("e.g. holdout of 0.70 means a 70-30 split for train/test data.\n")
 	exit(1)
 	
 def id3(df, t, f):
@@ -22,17 +33,28 @@ def id3(df, t, f):
 	Return:
 	root -- the fully formed decision tree
 	"""
-	root, ig = {}, {} # root note, dict of IG
-	for a in f: # for each attr
-		ig[a] = find_information_gain(df, t, a) # find IG for each attr split
-	highest_ig = f[np.argmax(ig)] # get best attr to split on
-	root = {highest_ig:{}} # make root node
-	split = make_split(df, highest_ig) # split root based on outcomes
-	for v in np.unique(df[highest_ig]):
-		df_branch = df.where(df[highest_ig] == v).dropna()
-		if len(df_branch) == 0:
-			return 
-		#branch = id3(df_branch, t, [i for i in f if i != highest_ig])
+	root, ig = {}, {} # root node, IG dict
+	attr = df.columns.drop(t) # get attribute set of df
+	for a in attr:
+		ig[a] = find_information_gain(df, t, a) # find IG of attr
+	highest_ig = max(ig, key=lambda key: ig[key]) # return key of highest val
+	s = make_split(df, highest_ig) # find splits on highest IG attr
+	root = {highest_ig:{}} # found root for further branches	
+	for v in s.keys(): # for each outcome of root
+		df_branch = df.where(df[highest_ig] == v).dropna() # remove root node
+		# if entropy of potential branch is zero, all outcomes same = term leaf
+		if find_entropy(df_branch[t]) == 0:
+			# add leaf branch
+			root[highest_ig][v] = s[v][t].value_counts().idxmax()
+		else: # otherwise branch has further subbranches = decision
+			if len(attr) - 1 == 0: # if no more attr to divide on
+				# entropy not 0, next branch isn't pure
+				root[highest_ig][v] = s[v][t].value_counts().idxmax()
+				return root
+			else: # if more attr to split on, can recurse
+				# recurse on split, dropping root attr
+				root[highest_ig][v] = id3(s[v].drop(highest_ig, axis=1), t, f)
+	return root
 	
 def find_entropy(t):
 	"""
@@ -72,16 +94,57 @@ def find_information_gain(df, t, s):
 	return total_h - split_h
 
 def make_split(df, t):
+	"""
+	Splits a dataframe on attribute.
+	
+	Parameter:
+	df -- the dataframe to split
+	t -- target attribute to split upon
+	
+	Return:
+	new_df -- split dataframe
+	"""
 	new_df = {}
 	for df_key in df.groupby(t).groups.keys():
 		new_df[df_key] = df.groupby(t).get_group(df_key)
 	return new_df
+
+def count_leaves(dt, c=[0,0]):
+	"""
+	Count number of non-leaf and leaf branches.
+	
+	Parameter:
+	dt -- the decision tree
+	c -- a counter
+	
+	Return:
+	c -- a count for both non-leeaves and leaves
+	"""
+	c[0] += 1
+	leaves = dt.keys()
+	for leaf in leaves:
+		branches = dt[leaf].values()
+		for branch in branches:
+			if isinstance(branch, dict):
+				count_leaves(branch, c)
+			else:
+				c[1] += 1
+	return c
 	
 def load_csv(f):
 	"""
 	Loads CSV file into pandas dataframe.
 	.CSV file is organized such that decision is the last column and features
 	are other columns. The first row is the name of decision and features.
+	
+	An example .CSV might be:
+	F1 F2 F3 F4 F5 F6 D
+	 0  0  1  1  0  1 1
+	 1  0  1  1  0  1 0
+	 1  0  0  0  1  1 0
+	 1  1  0  1  1  0 1
+	
+	Where F1..Fn are attributes and D is the decision
 	
 	Parameter:
 	f -- the filename for the .CSV file
@@ -90,10 +153,11 @@ def load_csv(f):
 	df -- a dataframe of examples
 	"""
 	try:
-		df = pd.read_csv(f) # open file as parse CSV into dataframe
+		df = pd.read_csv(f, dtype=str) # open file as parse CSV into dataframe
 	except:
 		print("\nData could not be loaded, ensure the argument is correct.\n")
 		exit(1)
+	print(f"\n{f} was loaded into dataframe.\n")
 	return df
 
 def holdout(df, p):
@@ -108,18 +172,105 @@ def holdout(df, p):
 	train -- training examples
 	test -- testing examples
 	"""
-	if 0.00 < p <= 1.00:
+	if 0.00 < p < 1.00:
 		d = df.copy()
 		train = d.sample(frac=p) # split, and randomize
 		test = d.drop(train.index) # remove train data from df
+		if len(test) == 0:
+			print("Proportion of training examples is too high.\n")
+			exit(1)
 		return train, test
 	else:
-		print("\nThe proportion of training examples must be (0.00..1.00].\n")
+		print("The proportion of training examples must be (0.00..1.00).\n")
 		exit(1)
 
+def find_accuracy(dt, t):
+	"""
+	Determines accuracy of the system.
+	
+	Parameter:
+	dt -- the decision tree
+	t -- a set of testing examples
+	
+	Return:
+	accuracy -- how accurate the system is
+	"""
+	correct, total = 0, 0
+	for _, e in t.iterrows():
+		total += 1
+		if e[len(e)-1] == predict_decision(dt, e):
+			correct += 1
+	return round(((correct/total)*100), 1)
+
+def predict_decision(dt, e):
+	"""
+	Predicts decision on a testing example.
+	
+	Parameter:
+	dt -- the decision tree
+	e -- a testing example
+	
+	Return:
+	decision -- a classification/decision
+	"""
+	split = list(dt.keys())[0]
+	try:
+		branch = dt[split][e[split]]
+	except KeyError:
+		return None
+	if not isinstance(branch, dict):
+		return branch
+	return predict_decision(branch, e)
+
+def print_tree(dt, indent=0):
+	"""
+	Prints decision tree in a better fashion.
+	
+	Parameter:
+	dt -- the tree to display
+	indent -- used internally for indentation
+	"""
+	for key, value in dt.items():
+		print("  " * indent + str(key))
+		if isinstance(value, dict): # if subdict
+			print_tree(value, indent+1)
+		else: # otherwise value
+			print("  " * (indent+1) + str(value))
+
+def print_statistics(dt, t, tr, te, trs, tes):
+	"""
+	Prints diagnostics regarding decision tree.
+	
+	Parameter:
+	dt -- the decision tree
+	t -- the time it took to generate dt
+	tr -- classification ability of training data
+	te -- classification ability of novel (test) data
+	trs -- number of training examples
+	tes -- number of testing examples
+	"""
+	s, d = count_leaves(dt) # splits and decisions
+	print(f"Using {trs} training examples and {tes} testing examples.")
+	print(f"Tree contains {s} non-leaf nodes and {d} leaf nodes.")
+	print("Took {:.2f} seconds to generate.".format(t))
+	print(f"Was able to classify {tr}% of training data.")
+	print(f"Was able to classify {te}% of testing data.\n")
+
 if __name__ == '__main__':
-	df = load_csv("./data/tennis.csv")
-	train, test = holdout(df, 1)
+	df = load_csv(argv[1]) # load CSV into dataframe
+	train, test = holdout(df, float(argv[2])) # get train/test split
 	decision_name = train.columns[len(train.columns)-1]
-	id3(train, decision_name, train.columns[:-1])
+	start_time = time.time()
+	dt = id3(train, decision_name, train.columns[:-1]) # get decision tree
+	end_time = time.time();
+	if int(argv[3]) == 1:
+		print(dt, "\n") # print decision tree as dict
+		print_tree(dt) # print decision tree
+		print()
+	t = end_time-start_time
+	tr_size = len(train)
+	te_size = len(test)
+	tr_ability = find_accuracy(dt, train)
+	te_ability = find_accuracy(dt, test)
+	print_statistics(dt, t, tr_ability, te_ability, tr_size, te_size)
 	exit(0)

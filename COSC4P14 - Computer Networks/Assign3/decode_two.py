8ec368c500c44f873c4b3f1222ea7aba44d2e142
stratzilla <stratzillab@gmail.com>
2020-03-26 15:36:25 -0400

    Fix

--- COSC4P14 - Computer Networks/Assign3/decode_two.py
+++ COSC4P14 - Computer Networks/Assign3/decode_two.py
@@ -5,7 +5,7 @@ from time import time
 
 if len(argv) != 2:
 	print("\nExecute the script as the below:")
-	print(" $ ./decode_one.py <File>\n")
+	print(" $ ./decode_two.py <File>\n")
 	print("Where <File> is the filename of the encoded file to decode.\n")
 	exit(1)
 
@@ -36,27 +36,46 @@ def determine_key(input):
 	Returns:
 		The key used to encrypt the file.
 	"""
-	print('\nBrute forcing key...')
+	print('\nBrute forcing key...\n')
 	best_key = 0
-	best_lfcr = 0
+	best_e = 0
 	start_t = time()
-	for potential_key in range(256): # brute force
-		if potential_key % 32 == 0:
-			print(f'Trying keys [{potential_key}, {potential_key+32}]...')
-		count_lfcr = 0
-		for i in range(len(input)-1): # every byte
-			# xor byte and next byte with key
-			# if this equals CR+LF, add to count
-			if input[i] ^ potential_key == 13 \
-			and input[i+1] ^ potential_key == 10:
-				count_lfcr += 1
-			if count_lfcr > best_lfcr:
+	for potential_key in range(32768): # brute force
+		if potential_key % 2048 == 0:
+			print(f'Trying keys [{potential_key}, {potential_key+2048}]...')
+		count_e = 0
+		for i in range(len(input)): # every byte
+			# xor byte with key
+			# if this equals `e` or `E`, add to count
+			key = key_generator(potential_key, i)
+			if input[i] ^ key == 69 \
+			or input[i] ^ key == 101:
+				count_e += 1
+			if count_e > best_e:
 				# keep track of the best count
-				best_lfcr = count_lfcr
+				best_e = count_e
 				best_key = potential_key
 	end_t = time()
 	print(f'\nBest key was {best_key}, found in {end_t-start_t:.2f}s.')
 	return best_key
+
+def key_generator(num, idx):
+	"""Generates keys from stream.
+	
+	Parameters:
+		num : the 15-bit word to generate 8-bit word from.
+		idx : how many cyclic bit shifts to perform.
+	
+	Returns:
+		The 15-bit word bit shifted idx times, sliced to 8-bits.
+	"""
+	bin_num = bin(num)[2:] # convert to bit string
+	for _ in range(15 - len(bin_num)):
+		bin_num = '0' + bin_num # pad so 15-bits
+	for _ in range(idx):
+		bin_num = bin_num[8:] + bin_num[:8] # shift
+	out_num = bin_num[:8] # get first 8 bits
+	return int('0b' + out_num, 2) # convert back
 
 def decode_file(input, key):
 	"""Decodes the file using the key.
@@ -67,8 +86,9 @@ def decode_file(input, key):
 	"""
 	print('\nSaving decoded file as \'decoded.txt\'...', end=' ')
 	for i in range(len(input)):
-		input[i] ^= key
-	open('decodedA.txt', 'wb').write(input)
+		the_key = key_generator(key, i)
+		input[i] ^= the_key
+	open('decodedB.txt', 'wb').write(input)
 	print('done!\n')
 
 if __name__ == '__main__':
--- COSC4P14 - Computer Networks/Assign3/encode_two.py
+++ COSC4P14 - Computer Networks/Assign3/encode_two.py
@@ -4,7 +4,7 @@ from sys import argv, exit
 
 if len(argv) != 2:
 	print("\nExecute the script as the below:")
-	print(" $ ./encode_one.py <File>\n")
+	print(" $ ./encode_two.py <File>\n")
 	print("Where <File> is the filename of the plaintext file to encode.\n")
 	exit(1)
 
@@ -33,14 +33,32 @@ def encode_file(input, key):
 		input : the bytearray of the file.
 		key : the key used to encode it.
 	"""
-	print('\nSaving encoded file as \'rs09co.answer.A.enc\'...', end=' ')
+	print('\nSaving encoded file as \'rs09co.answer.B.enc\'...', end=' ')
 	for i in range(len(input)):
-		input[i] ^= key
-	open('rs09co.answer.A.enc', 'wb').write(input)
+		input[i] ^= key_generator(key, i)
+	open('rs09co.answer.B.enc', 'wb').write(input)
 	print('done!\n')
+
+def key_generator(num, idx):
+	"""Generates keys from stream.
+	
+	Parameters:
+		num : the 15-bit word to generate 8-bit word from.
+		idx : how many cyclic bit shifts to perform.
+	
+	Returns:
+		The 15-bit word bit shifted idx times, sliced to 8-bits.
+	"""
+	bin_num = bin(num)[2:] # convert to bit string
+	for _ in range(15 - len(bin_num)):
+		bin_num = '0' + bin_num # pad so 15-bits
+	for _ in range(idx):
+		bin_num = bin_num[8:] + bin_num[:8] # shift
+	out_num = bin_num[:8] # get first 8 bits
+	return int('0b' + out_num, 2) # convert back
 
 if __name__ == '__main__':
 	FILE = load_file(argv[1])
-	KEY = 100 + 42
+	KEY = 4582
 	encode_file(FILE, KEY)
 	exit(0)

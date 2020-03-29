f096869ffd5829083f986327f0fece1d4cfa69aa
stratzilla <stratzillab@gmail.com>
2020-03-29 16:30:36 -0400

    Flaired with better comments

--- COSC4P14 - Computer Networks/Assign3/decode_one.py
+++ COSC4P14 - Computer Networks/Assign3/decode_one.py
@@ -3,75 +3,73 @@
 from sys import argv, exit
 from time import time
 
+# if inappropriate argument(s), remind user on how to execute
 if len(argv) != 2:
-	print("\nExecute the script as the below:")
-	print(" $ ./decode_one.py <File>\n")
-	print("Where <File> is the filename of the encoded file to decode.\n")
+	print('\nExecute the script as the below:')
+	print(' $ ./decode_one.py <File>\n')
+	print('Where <File> is the filename of the encoded file to decode.\n')
 	exit(1)
 
-def load_file(input):
-	"""Loads the argumented file as bytearray.
-	
-	Parameters:
-		input : the filename to open.
-	
-	Returns:
-		A bytearray from the input.
-	"""
-	print(f'\nLoading {input}...', end=' ')
-	try:
-		file = bytearray(open(input, 'rb').read())
-		print('success!')
-		return file
-	except:
-		print('failed!\n')
-		exit(1)
-
-def determine_key(input):
+def determine_key(ba):
 	"""Determine the key used to encrypt the file.
 	
 	Parameters:
-		input : the bytearray of the file
+		ba : the bytearray of the file
 		
 	Returns:
 		The key used to encrypt the file.
 	"""
 	print('\nBrute forcing key...\n')
-	best_key = 0
-	best_lfcr = 0
-	start_t = time()
+	best_key, best_lfcr = 0, 0 # keep track of the best key and best lfcr count
+	start_t = time() # time how long it took
 	for potential_key in range(256): # brute force
 		if potential_key % 32 == 0:
 			print(f'Trying keys [{potential_key}, {potential_key+31}]...')
-		count_lfcr = 0
-		# since it is Windows encoded, there will be an abundance of
-		# CR,LF found in the file. Find a decoding which maximizes this
-		for i in range(len(input)-1): # every byte
-			# xor byte and next byte with key
-			# if this equals CR+LF, add to count
-			if input[i] ^ potential_key == 13 \
-			and input[i+1] ^ potential_key == 10:
+		count_lfcr = 0 # to count occurrences of CR+LF
+		# since the file is Windows encoded, it is safe to assume that the
+		# occurrence of CR followed by LF is high. Try to decode file based on
+		# the currently iterated key, and then count the occurrences of CR+LF
+		for i in range(len(ba)-1): # for every byte
+			# xor byte and next byte with key, if this equals CR+LF, count++
+			if ba[i] ^ potential_key == 13 and ba[i+1] ^ potential_key == 10:
 				count_lfcr += 1
-			if count_lfcr > best_lfcr:
-				# keep track of the best count
-				best_lfcr = count_lfcr
-				best_key = potential_key
-	end_t = time()
+		if count_lfcr > best_lfcr: # if better than previously found
+			best_lfcr = count_lfcr # reinitialize as new highest
+			best_key = potential_key # keep track of key that gave highest
+	end_t = time() # finish timing
 	print(f'\nBest key was {best_key}, found in {end_t-start_t:.2f}s.')
 	return best_key
 
-def decode_file(input, key):
+def decode_file(ba, key):
 	"""Decodes the file using the key.
 	
 	Parameters:
-		input : the bytearray of the file.
+		ba : the bytearray of the file.
 		key : the key used to decode it.
 	"""
 	print('\nSaving decoded file as \'../outputs/decodedA.txt\'...', end=' ')
-	for i in range(len(input)):
-		input[i] ^= key
-	open('../outputs/decodedA.txt', 'wb').write(input)
+	for i in range(len(ba)): # for each byte in bytearray
+		ba[i] ^= key # xor it with found key
+	open('../outputs/decodedA.txt', 'wb').write(ba)
 	print('done!\n')
+	
+def load_file(filename):
+	"""Loads the argumented file as bytearray.
+	
+	Parameters:
+		filename : the filename to open.
+	
+	Returns:
+		A bytearray from the input.
+	"""
+	print(f'\nLoading {filename}...', end=' ')
+	try: # attempt to load file
+		file = bytearray(open(filename, 'rb').read()) # load as bytearray
+		print('success!')
+		return file
+	except: # catch IOErrors, FileNotFoundErrors, etc
+		print('failed!\n')
+		exit(1)
 
 if __name__ == '__main__':
 	FILE = load_file(argv[1])
--- COSC4P14 - Computer Networks/Assign3/decode_two.py
+++ COSC4P14 - Computer Networks/Assign3/decode_two.py
@@ -3,64 +3,43 @@
 from sys import argv, exit
 from time import time
 
+# if inappropriate argument(s), remind user on how to execute
 if len(argv) != 2:
-	print("\nExecute the script as the below:")
-	print(" $ ./decode_two.py <File>\n")
-	print("Where <File> is the filename of the encoded file to decode.\n")
+	print('\nExecute the script as the below:')
+	print(' $ ./decode_two.py <File>\n')
+	print('Where <File> is the filename of the encoded file to decode.\n')
 	exit(1)
 
-def load_file(input):
-	"""Loads the argumented file as bytearray.
-	
-	Parameters:
-		input : the filename to open.
-	
-	Returns:
-		A bytearray from the input.
-	"""
-	print(f'\nLoading {input}...', end=' ')
-	try:
-		file = bytearray(open(input, 'rb').read())
-		print('success!')
-		return file
-	except:
-		print('failed!\n')
-		exit(1)
-
-def determine_key(input):
+def determine_key(ba):
 	"""Determine the key used to encrypt the file.
 	
 	Parameters:
-		input : the bytearray of the file
+		ba : the bytearray of the file
 		
 	Returns:
 		The key used to encrypt the file.
 	"""
 	print('\nBrute forcing key...\n')
-	best_key = 0
-	best_e = 0
-	start_t = time()
+	best_key, best_e = 0, 0 # keep track of the best key and best e count
+	start_t = time() # time how long it took
 	for potential_key in range(32768): # brute force
 		if potential_key % 4096 == 0:
 			print(f'Trying keys [{potential_key}, {potential_key+4095}]...')
-		count_e = 0
-		# consider the most correct decoding to be one which
-		# follows English letter frequency, namely for 'e'/'E'
-		# in English, 'e'/'E' appears roughly 11% of the time, more
-		# than any other letter. It can be assumed if the most frequent
-		# letter is 'e'/'E', then the decoding is correct
-		for i in range(len(input)): # every byte
-			# xor byte with key
-			# if this equals `e` or `E`, add to count
-			key = key_generator(potential_key, i)
-			if input[i] ^ key == 69 \
-			or input[i] ^ key == 101:
+		count_e = 0 # to count occurrences of 'e'/'E'
+		# since the file is English text, it is safe to assume that it also
+		# follows English letter frequency. The most abundant letter in the
+		# English language is 'E' and 'e', accounting for roughly 11% of all
+		# letters used. Try to decode the file based on the currently iterated
+		# key, and then count the occurrences of 'E' or 'e'
+		for i in range(len(ba)): # for every byte
+			key = key_generator(potential_key, i) # generate next key
+			# xor byte with key, if this equals `e` or `E`, add to count
+			if ba[i] ^ key == 69 or ba[i] ^ key == 101:
 				count_e += 1
-			if count_e > best_e:
-				# keep track of the best count
-				best_e = count_e
-				best_key = potential_key
-	end_t = time()
+		if count_e > best_e: # if better than previously found
+			best_e = count_e # reinitialize as new highest
+			best_key = potential_key # keep track of key that gave highest
+	end_t = time() # finish timing
 	print(f'\nBest key was {best_key}, found in {end_t-start_t:.2f}s.')
 	return best_key
 
@@ -92,19 +71,37 @@ def key_generator(num, idx):
 		bin_num = bin_num[8:] + bin_num[:8] # shift
 	return int('0b' + bin_num[:8], 2) # convert back
 
-def decode_file(input, key):
+def decode_file(ba, key):
 	"""Decodes the file using the key.
 	
 	Parameters:
-		input : the bytearray of the file.
+		ba : the bytearray of the file.
 		key : the key used to decode it.
 	"""
 	print('\nSaving decoded file as \'../outputs/decodedB.txt\'...', end=' ')
-	for i in range(len(input)):
-		the_key = key_generator(key, i)
-		input[i] ^= the_key
-	open('../outputs/decodedB.txt', 'wb').write(input)
+	for i in range(len(ba)): # for each byte in bytearray
+		the_key = key_generator(key, i) # generate next key
+		ba[i] ^= the_key # xor it with found key
+	open('../outputs/decodedB.txt', 'wb').write(ba)
 	print('done!\n')
+
+def load_file(filename):
+	"""Loads the argumented file as bytearray.
+	
+	Parameters:
+		filename : the filename to open.
+	
+	Returns:
+		A bytearray from the input.
+	"""
+	print(f'\nLoading {filename}...', end=' ')
+	try: # attempt to load file
+		file = bytearray(open(filename, 'rb').read()) # load as bytearray
+		print('success!')
+		return file
+	except: # catch IOErrors, FileNotFoundErrors, etc
+		print('failed!\n')
+		exit(1)
 
 if __name__ == '__main__':
 	FILE = load_file(argv[1])

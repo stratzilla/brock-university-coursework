09535b7a2094c280cae084ea868ff4aa190801e1
Rob Scott <stratzillab@gmail.com>
2021-11-14 11:50:49 -0500

    Update Test3.hs

--- COSC4P41 - Functional Programming/Test3.hs
+++ COSC4P41 - Functional Programming/Test3.hs
@@ -3,7 +3,7 @@ data Stack a = Stack [a] deriving Show
 empty :: Stack a
 empty = Stack []
 
-isEmpty :: Stack a -> Bool a
+isEmpty :: Stack a -> Bool
 isEmpty (Stack xs) = null xs
 
 push :: Stack a -> a -> Stack a
@@ -35,17 +35,17 @@ exampleProg2 = [PUSH 3, PUSH 4, ADD, DIV]
 evalSMInst :: Stack Int -> SMInst -> Maybe (Stack Int)
 evalSMInst (Stack xs) (PUSH n) = Just (push (Stack xs) n)
 evalSMInst (Stack xs) (ADD) = case pop2 (Stack xs) of
-	Nothing -> Nothing
-	Just ((x1, x2), xs) -> Just (push xs (x1+x2))
+    Nothing -> Nothing
+    Just ((x1, x2), (Stack xs)) -> Just (push (Stack xs) (x1+x2))
 evalSMInst (Stack xs) (MINUS) = case pop2 (Stack xs) of
-	Nothing -> Nothing
-	Just ((x1, x2), xs) -> Just (push xs (x1-x2))
+    Nothing -> Nothing
+    Just ((x1, x2), (Stack xs)) -> Just (push (Stack xs) (x1-x2))
 evalSMInst (Stack xs) (MULT) = case pop2 (Stack xs) of
-	Nothing -> Nothing
-	Just ((x1, x2), xs) -> Just (push xs (x1*x2))
+    Nothing -> Nothing
+    Just ((x1, x2), (Stack xs)) -> Just (push (Stack xs) (x1*x2))
 evalSMInst (Stack xs) (DIV) = case pop2 (Stack xs) of
-	Nothing -> Nothing
-	Just ((x1, x2), xs) -> Just (push xs (x1 `div` x2))
+    Nothing -> Nothing
+    Just ((x1, x2), (Stack xs)) -> Just (push (Stack xs) (x1 `div` x2))
 
 evalSMProg :: Stack Int -> SMProg -> Maybe (Stack Int)
 evalSMProg (Stack xs) p = Nothing

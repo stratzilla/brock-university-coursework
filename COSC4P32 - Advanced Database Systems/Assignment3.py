575b083d9b5974bc5d8a897751c80fe5c4141855
Rob Scott <stratzillab@gmail.com>
2022-04-05 17:39:02 -0400

    Update Assignment3.py

--- COSC4P32 - Advanced Database Systems/Assignment3.py
+++ COSC4P32 - Advanced Database Systems/Assignment3.py
@@ -40,64 +40,76 @@ def fetchNext(self): # NestedLoopJoinExecutor
 	return None # if it gets here, was EOF for LHS and RHS
 
 def fetchGenerator(self):
-    # treat map as generator and yield rows
-    for row in self.group_map:
-        yield row
+	# treat map as generator and yield rows
+	for row in self.group_map:
+		yield row
 
 def fetchNext(self): # AggregatorExecutor
-    # on first fetch, generate the whole map first
-    if not self.group_map: # if the map has not yet been added to
-        self.fetchGen = self.fetchGenerator() # initialize the generator
-        group_by = self.plan.getGroupByAt() # get name of group-by column
-        agg_at = self.plan.getAggregateAt() # get name of aggregate column
-		exec_schema = self.child_executor.plan.getOutputSchema() # get schema
-		group_idx = exec_schema.getColunmIdx(group_by) # get index for group col
-		agg_idx = exec_schema.getColunmIdx(agg_at) # same for aggregate col
-        curr = self.child_executor.fetchNext() # keep track of current tuple
-        while curr is not None: # while not EOF
-            # add to map
-            self.insert(curr[group_idx], curr[agg_idx])
-            curr = self.child_executor.fetchNext() # go to next
-    # use a try/catch just in case of error since can't tell if gen is empty
-    try:
-        val = next(self.fetchGen) # get the next yielded row from generator
-    except StopIteration: # if null
-        return None # you've reached EOF
-    return (val, self.group_map[val]) # otherwise return the row
+	# on first fetch, generate the whole map first
+	if not self.group_map: # if the map has not yet been added to
+		self.fetchGen = self.fetchGenerator() # initialize the generator
+		group_by = self.plan.getGroupByAt() # get name of group-by column
+		agg_at = self.plan.getAggregateAt() # get name of aggregate column
+		# get corresponding indices in schema
+		group_idx = self.child_executor.plan.getOutputSchema().getColunmIdx(group_by)
+		agg_idx = self.child_executor.plan.getOutputSchema().getColunmIdx(agg_at)
+		curr = self.child_executor.fetchNext() # keep track of currently iterated tuple
+		while curr is not None: # while not EOF
+			# add to map
+			self.insert(curr[group_idx], curr[agg_idx])
+			curr = self.child_executor.fetchNext() # go to next
+	# since you can't tell if a generator is empty, need a try/catch to handle error
+	try:
+		key = next(self.fetchGen) # get the next yielded row from generator
+	except StopIteration: # if null
+		return None # you've reached EOF
+	agg_type = self.plan.getAggregateType() # find out what exactly to return
+	# and set the value to whatever that value is to return
+	if agg_type == "MIN":
+		value = self.group_map[key].getMin()
+	elif agg_type == "MAX":
+		value = self.group_map[key].getMax()
+	elif agg_type == "SUM":
+		value = self.group_map[key].getSum()
+	elif agg_type == "AVG":
+		value = self.group_map[key].getAvg()
+	elif agg_type == "COUNT":
+		value = self.group_map[key].getCount()
+	return (key, value) # otherwise return the row
 
 def fetchNext(self):
-    group_by = self.plan.getGroupByAt() # get name of group-by column
-    agg_at = self.plan.getAggregateAt() # get name of aggregate column
+	group_by = self.plan.getGroupByAt() # get name of group-by column
+	agg_at = self.plan.getAggregateAt() # get name of aggregate column
 	exec_schema = self.child_executor.plan.getOutputSchema() # get schema
-    group_idx = exec_schema.getColunmIdx(group_by) # get index for group col
-    agg_idx = exec_schema.getColunmIdx(agg_at) # same for aggregate col
-    self.child_executor.reset() # reset the executor to seek 0
-    target = self.child_executor.fetchNext() # first tuple target to group by
-    if target is None: # if EOF, return None to stop executor
-        return None
-    '''
-    The aggregate result is iteratively built up, but we don't want to
-    waste time retrying tuples already added and aggregated. To this
-    end, we want to iterate the target tuple until we find one that
-    doesn't already exist in the aggregate map.
+	group_idx = exec_schema.getColunmIdx(group_by) # get index for group col
+	agg_idx = exec_schema.getColunmIdx(agg_at) # same for aggregate col
+	self.child_executor.reset() # reset the executor to seek 0
+	target = self.child_executor.fetchNext() # first tuple target to group by
+	if target is None: # if EOF, return None to stop executor
+		return None
+	'''
+	The aggregate result is iteratively built up, but we don't want to
+	waste time retrying tuples already added and aggregated. To this
+	end, we want to iterate the target tuple until we find one that
+	doesn't already exist in the aggregate map.
 
-    Once we have a target, we look at tuples AFTER the target, because
-    rows before the target would have been already aggregated. Technically
-    the algorithm should speed up the longer it goes on because it will pass
-    over tuples that share the same group index.
-    '''
-    while target[group_idx] in self.group_map and target is not None:
-        target = self.child_executor.fetchNext()
-    # if target is not in the map, it's safe to assume it hasn't been seen
-    if target[group_idx] not in self.group_map:
-        # so add it to the map
-        self.insert(target[group_idx], target[agg_idx])
-        # then iterate over tuples succeeding the target
-        curr = self.child_executor.fetchNext()
-        while curr is not None: # while not EOF
-            # if match to target, increment its aggregate
-            if curr[group_idx] == target[group_idx]:
-                self.insert(target[group_idx], curr[agg_idx])
-            curr = self.child_executor.fetchNext() # go to next
-        # at the end of this, return the aggregate data for that group
-        return (target[group_idx], self.group_map[target[group_idx]])
+	Once we have a target, we look at tuples AFTER the target, because
+	rows before the target would have been already aggregated. Technically
+	the algorithm should speed up the longer it goes on because it will pass
+	over tuples that share the same group index.
+	'''
+	while target[group_idx] in self.group_map and target is not None:
+		target = self.child_executor.fetchNext()
+	# if target is not in the map, it's safe to assume it hasn't been seen
+	if target[group_idx] not in self.group_map:
+		# so add it to the map
+		self.insert(target[group_idx], target[agg_idx])
+		# then iterate over tuples succeeding the target
+		curr = self.child_executor.fetchNext()
+		while curr is not None: # while not EOF
+			# if match to target, increment its aggregate
+			if curr[group_idx] == target[group_idx]:
+				self.insert(target[group_idx], curr[agg_idx])
+			curr = self.child_executor.fetchNext() # go to next
+		# at the end of this, return the aggregate data for that group
+		return (target[group_idx], self.group_map[target[group_idx]])
